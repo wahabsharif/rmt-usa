b24ccbcbec30409106a37afed798703b144fcd53
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-22 09:41:04 +0500

    NavBar Updated

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -5,14 +5,14 @@ import { navBarData } from "@/data/navBarData";
 import Link from "next/link";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
-import { TiArrowSortedUp } from "react-icons/ti"; // Import the arrow icon
+import { TiArrowSortedUp } from "react-icons/ti";
 import ThemeToggle from "./ThemeToggle";
 
 const NavBar: React.FC = () => {
   const [submenuOpenIndex, setSubmenuOpenIndex] = useState<number | null>(null);
 
   return (
-    <nav className="bg-gray-100 dark:bg-gray-800/70 backdrop-blur-md fixed top-8 left-1/2 transition ease-in-out transform -translate-x-1/2 w-[95%] z-50 rounded-xl shadow-lg">
+    <nav className="bg-gray-100 dark:bg-gray-800/70 backdrop-blur-md fixed top-8 left-1/2 transform -translate-x-1/2 w-[95%] z-50 rounded-xl shadow-lg hidden md:block transition ease-in-out">
       <div className="container mx-auto flex items-center justify-between p-4">
         {/* Left Side: Logo */}
         <Link href="/">
@@ -37,12 +37,12 @@ const NavBar: React.FC = () => {
                   menuItem.submenu && setSubmenuOpenIndex(index)
                 }
                 onMouseLeave={() => setSubmenuOpenIndex(null)}
-                whileHover={{ scale: 1.1 }} // Zoom in effect on hover
+                whileHover={{ scale: 1.1 }}
                 transition={{ type: "spring", stiffness: 300 }}
               >
                 <Link href={menuItem.href}>
                   <motion.div
-                    whileHover={{ scale: 1 }} // Slightly larger zoom on hover
+                    whileHover={{ scale: 1 }}
                     transition={{ type: "spring", stiffness: 200 }}
                     className="flex items-center "
                   >
@@ -50,11 +50,11 @@ const NavBar: React.FC = () => {
                     {/* Arrow Icon - Appears when submenu exists */}
                     {menuItem.submenu && (
                       <motion.div
-                        className="ml-1" // Margin to the left for spacing
+                        className="ml-1"
                         initial={{ rotate: 0 }}
                         animate={{
                           rotate: submenuOpenIndex === index ? 180 : 0,
-                        }} // Rotate only on hover
+                        }}
                         transition={{ duration: 0.3 }}
                       >
                         <TiArrowSortedUp />
