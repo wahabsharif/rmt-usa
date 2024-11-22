a3d79c0ff092803e271ef3cba95c60c4cec4d7f3
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-22 08:03:35 +0500

    Dual Theme

---
+++ src/components/common/ThemeToggle.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { FaMoon } from "react-icons/fa";
+import { LuSunMedium } from "react-icons/lu";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
+
+const ThemeToggle = () => {
+  // Theme state (light or dark)
+  const [theme, setTheme] = useState("light");
+
+  // Toggle between light and dark mode
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+    localStorage.setItem("theme", newTheme);
+  };
+
+  // Check and apply saved theme from localStorage
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    setTheme(savedTheme);
+    document.documentElement.classList.toggle("dark", savedTheme === "dark");
+  }, []);
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-xl focus:outline-none bg-gray-700"
+    >
+      <AnimatePresence mode="wait" initial={false}>
+        {theme === "light" ? (
+          <motion.div
+            key="sun"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.5 }}
+            transition={{ duration: 0.2 }}
+          >
+            <LuSunMedium size={24} />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="moon"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.5 }}
+            transition={{ duration: 0.2 }}
+          >
+            <FaMoon size={24} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </button>
+  );
+};
+
+export default ThemeToggle;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,6 +10,8 @@ export default {
   theme: {
     extend: {
       colors: {
+        thDarkBlue: "#013550",
+        thLightBlue: "#00b6c6",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
